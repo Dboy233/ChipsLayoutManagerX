da8d73d3120e868a322989a7c7d6045fb349313d
Dboy <894230813@qq.com>
2020-12-15 15:37:28 +0800

    清除Log

--- library/src/main/java/com/dboy/chips/Log.java
+++ library/src/main/java/com/dboy/chips/Log.java
@@ -3,39 +3,41 @@ package com.dboy.chips;
 import java.util.HashSet;
 import java.util.Set;
 
-/** this class with static methods created only for fast replace of default android log */
+/**
+ * this class with static methods created only for fast replace of default android log
+ */
 public class Log {
 
     private static LogSwitcher logSwitcher = new LogSwitcher();
 
     @SuppressWarnings("ConstantConditions")
-    private static  LogWrapper log = true ? new  AndroidLog() : new SilentLog();
+    private static final LogWrapper log = BuildConfig.DEBUG ? new AndroidLog() : new SilentLog();
 
     ///////////////////////////////////////////////////////////////////////////
     // default android log delegates
     ///////////////////////////////////////////////////////////////////////////
 
     @SuppressWarnings("WeakerAccess")
-    public static int d (String tag, String msg) {
+    public static int d(String tag, String msg) {
         return log.d(tag, msg);
     }
 
     @SuppressWarnings("WeakerAccess")
-    public static int v (String tag, String msg) {
+    public static int v(String tag, String msg) {
         return log.v(tag, msg);
     }
 
     @SuppressWarnings("WeakerAccess")
-    public static int w (String tag, String msg) {
+    public static int w(String tag, String msg) {
         return log.w(tag, msg);
     }
 
     @SuppressWarnings("WeakerAccess")
-    public static int i (String tag, String msg) {
+    public static int i(String tag, String msg) {
         return log.i(tag, msg);
     }
 
-    public static int e (String tag, String msg) {
+    public static int e(String tag, String msg) {
         return log.e(tag, msg);
     }
 
@@ -44,22 +46,22 @@ public class Log {
     ///////////////////////////////////////////////////////////////////////////
 
     @SuppressWarnings("WeakerAccess")
-    public static int d (String tag, String msg, int logCode) {
+    public static int d(String tag, String msg, int logCode) {
         return logSwitcher.isEnabled(logCode) ? d(tag, msg) : 0;
     }
 
     @SuppressWarnings("WeakerAccess")
-    public static int v (String tag, String msg, int logCode) {
+    public static int v(String tag, String msg, int logCode) {
         return logSwitcher.isEnabled(logCode) ? v(tag, msg) : 0;
     }
 
     @SuppressWarnings("WeakerAccess")
-    public static int w (String tag, String msg, int logCode) {
+    public static int w(String tag, String msg, int logCode) {
         return logSwitcher.isEnabled(logCode) ? w(tag, msg) : 0;
     }
 
     @SuppressWarnings("WeakerAccess")
-    public static int i (String tag, String msg, int logCode) {
+    public static int i(String tag, String msg, int logCode) {
         return logSwitcher.isEnabled(logCode) ? i(tag, msg) : 0;
     }
 
@@ -68,7 +70,7 @@ public class Log {
     }
 
     public static class LogSwitcher {
-        private Set<Integer> enabledLogs = new HashSet<>();
+        private final Set<Integer> enabledLogs = new HashSet<>();
 
         boolean isEnabled(int logCode) {
             return enabledLogs.contains(logCode);
